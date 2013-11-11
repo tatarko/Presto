27ad4f92f9324a32168c10d526fbb5bf38499887
Tomáš Tatarko <tomas@tatarko.sk>
2013-11-11 21:36:29 +0100

    LayoutView class refactored

---
+++ source/LayoutView.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Tomáš Tatarko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Presto;
+
+/**
+ * Extented View due to differed layout view path
+ * @package Presto
+ * @author Tomas Tatarko <tomas@tatarko.sk>
+ * @copyright (c) 2013, Tomas Tatarko
+ * @link https://github.com/tatarko/Presto
+ * @license http://choosealicense.com/licenses/mit/ The MIT License
+ * @since 1.0
+ */
+class LayoutView extends View
+{
+
+    /**
+     * Gets file real path of layout view
+     * @param string $name
+     * @return string
+     */
+    public function getFilePath($name)
+    {
+        return sprintf('%slayouts/%s.tpl', $this->template->path, $name);
+    }
+}
